a2dbc74c025cd9d7039edd9f6e0eb95a988a5d12
takumaro-web <ta7ku2ma3@gmail.com>
2014-03-28 19:25:24 +0900

    Delete yoshufukushu.js

--- common/js/yoshufukushu.js
+++
@@ -1,9 +0,0 @@
-function() {
-
-	$("h1").children().andSelf().contents().each(function() {
-    	if (this.nodeType == 3) {
-        	$(this).replaceWith($(this).text().replace(/(\S)/g, '<span>$1</span>'));
-    	}
-	});
-
-});
